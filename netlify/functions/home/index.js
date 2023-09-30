1041cb751e11f883c12088fa4ec80f15952435f1
Patrick Sara <patricktheodoresara@gmail.com>
2023-09-30 17:32:33 +0800

    updated URL

--- netlify/functions/home/index.js
+++ netlify/functions/home/index.js
@@ -14,9 +14,6 @@ export async function handler(event, context) {
 
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
       body: JSON.stringify(response)
     };
   } catch (error) {
