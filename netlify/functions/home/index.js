0c17aece94ff5bdb3eec1c27b0f9ffce83f5f368
Patrick Sara <patricktheodoresara@gmail.com>
2023-09-30 17:13:32 +0800

    attempting to fix home function

--- netlify/functions/home/index.js
+++ netlify/functions/home/index.js
@@ -14,6 +14,9 @@ export async function handler(event, context) {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
       body: JSON.stringify(response)
     };
   } catch (error) {
